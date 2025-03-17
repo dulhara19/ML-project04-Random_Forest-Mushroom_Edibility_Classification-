e2e08743e9f7a67c6ea7a7b6829bcd840f0694f8
dulhara <wkldulhara@gmail.com>
2025-03-17 23:26:11 +0530

    converted ds into df

--- model.py
+++ model.py
@@ -1,7 +1,11 @@
 from datasets import load_dataset
+import pandas as pd
 
 # Load dataset
-dataset = load_dataset("imdb")
+dataset = load_dataset("jlh/uci-mushrooms")
 
 # Check available splits
 print(dataset)
+
+df = pd.DataFrame(dataset ['train'])
+print(df)
